9b88b49f093e6b72fb64a6e52c06aef1b57e952e
Anusreesm <162425600+Anusreesm@users.noreply.github.com>
2024-08-17 20:40:04 +0530

    Delete card.jsx

--- card.jsx
+++
@@ -1,9 +0,0 @@
-function Card(props){
-    return (
-        <article classname="country-div">
-        <img src={props.countryFlag} alt="countryFlag"/>
-        <h2>{props.countryName}</h2>
-        </article>
-    );
-}
-export default Card;
